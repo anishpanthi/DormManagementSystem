b824681657b9b78073b72ba9be7ef2fd3fa9753b
Umesh Paudyal <umesh.paudel.nepal@gmail.com>
2016-05-18 20:06:10 -0500

    commented the unused method

--- src/main/java/com/apub/dorm/service/impl/RoomServiceImpl.java
+++ src/main/java/com/apub/dorm/service/impl/RoomServiceImpl.java
@@ -38,15 +38,15 @@ public class RoomServiceImpl implements RoomService {
 		roomRepository.save(room);
 	}
 
-	@Override
+	/*@Override
 	public List<Room> findRoomNumbersByBuildingNo(int buildingNumber) {
-		/*List<Room> result = new ArrayList<>();
+		List<Room> result = new ArrayList<>();
 		for(Room room : roomRepository.findAll()) {
 			if(room.getBuildingNo() == buildingNumber) result.add(room);
-		}*/
+		}
 		System.out.println(buildingNumber+" ===== "+roomRepository.findRoomNumbersByBuildingNo(buildingNumber).toString());
 		return roomRepository.findRoomNumbersByBuildingNo(buildingNumber);
-	}
+	}*/
 
 	@Override
 	public List<Item> getAllItems() {
